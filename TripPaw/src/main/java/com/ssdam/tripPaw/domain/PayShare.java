771ab7d37a32782fdf2df4eed6810394643fd41c
Lee-jaemyeong <kinglee98@naver.com>
2025-06-27 15:43:47 +0900

    2025_06_27 Team_Project3 _Day 1 예약/결제 테이블

---
+++ TripPaw/src/main/java/com/ssdam/tripPaw/domain/PayShare.java
@@ -0,0 +1,43 @@
+package com.ssdam.tripPaw.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class PayShare {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+    private int amount;
+    private Boolean hasPaid;
+    private LocalDateTime paidAt;
+    private LocalDateTime createdAt;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reserv_id", nullable = false)
+    private Reserv reserv;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pay_id", nullable = false)
+    private Pay pay;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payGroup_id", nullable = false)
+    private PayGroup payGroup;
+}
