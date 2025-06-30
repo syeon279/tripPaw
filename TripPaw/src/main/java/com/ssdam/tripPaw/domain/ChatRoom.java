9c067ac4a2fa3c8b7110d2b1b8df53fd2f54a869
yoon0416 <zgha16@gmail.com>
2025-06-30 10:46:12 +0900

    2025-06-30 chat 수정

--- TripPaw/src/main/java/com/ssdam/tripPaw/domain/ChatRoom.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/domain/ChatRoom.java
@@ -5,7 +5,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -17,8 +21,12 @@ import lombok.Data;
 @Table(name = "ChatRoom") // DB 테이블명과 맞추기
 @Data
 public class ChatRoom {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(nullable = false, length = 255)
 	private String title;
+	@Column(nullable = false, length = 255)
 	private String description;
 	private boolean isGroup;
 	private LocalDateTime createdAt = LocalDateTime.now();
