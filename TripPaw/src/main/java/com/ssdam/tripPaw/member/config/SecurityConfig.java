2a52677b835f6558cdfdd86309e3f343de526056
Lee-jaemyeong <kinglee98@naver.com>
2025-07-01 14:32:12 +0900

    Update SecurityConfig

--- TripPaw/src/main/java/com/ssdam/tripPaw/member/config/SecurityConfig.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/member/config/SecurityConfig.java
@@ -17,14 +17,11 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable()) // CSRF 보호 비활성화 (WebSocket과 함께 사용할 때 필요할 수 있음)
             .authorizeHttpRequests(authz -> authz
-<<<<<<< HEAD
                 //.antMatchers("/**").permitAll() // "/**"는 모든 경로를 의미합니다.
                 //.antMatchers("/admin/**").hasRole("ADMIN") // 관리자만 접근 가능 <= 이거 추가
-                .antMatchers("/admin/**").permitAll() // ← 개발 중이므로 임시 허용
-=======
                 .antMatchers("/**").permitAll() // "/**"는 모든 경로를 의미합니다.
                 .antMatchers("/admin/**").hasRole("ADMIN") // 관리자만 접근 가능 <= 이거 추가
->>>>>>> 6a52e51402e1316d7ed468e0bf9164578656b200
+
                 .anyRequest().authenticated()
             );
 
