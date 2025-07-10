0e8d044f5ebaa6692a146d412f5e565bda534517
yeleem <dpflaalee@naver.com>
2025-07-10 23:02:50 +0900

    no message

--- TripPaw/src/main/java/com/ssdam/tripPaw/domain/Member.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/domain/Member.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Flow.Subscription;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
