52da6ce32be6ea3e6209b6a2ea6549fd157a4f12
sohyeon <syeonblue@gmail.com>
2025-07-25 11:15:32 +0900

    Update Seal.java

--- TripPaw/src/main/java/com/ssdam/tripPaw/domain/Seal.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/domain/Seal.java
@@ -19,7 +19,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity @Getter @Setter @NoArgsConstructor
-public class Seal {
+public class Seal { 
+	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
@@ -27,7 +28,7 @@ public class Seal {
 	private String name;
 	private String imageUrl;
 	
-	//장소연결필요
+	//장소연결
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "placetype_id", nullable = false)
 	private PlaceType placeType;
@@ -35,4 +36,5 @@ public class Seal {
 	//여권-도장 연결
 	@OneToMany(mappedBy = "seal", cascade = CascadeType.ALL,  orphanRemoval = true)
 	private List<PassportSeal> PassportSeals = new ArrayList<>();
+	
 }
