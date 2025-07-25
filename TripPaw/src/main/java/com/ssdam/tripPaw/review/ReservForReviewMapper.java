71a6837e566374b44f409b6fea03acd47cbe69d3
sohyeon <syeonblue@gmail.com>
2025-07-25 12:26:43 +0900

    Update ReservForReviewMapper.java

--- TripPaw/src/main/java/com/ssdam/tripPaw/review/ReservForReviewMapper.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/review/ReservForReviewMapper.java
@@ -17,8 +17,6 @@ public interface ReservForReviewMapper {
     int countByMemberAndTripPlan(@Param("memberId") Long memberId, @Param("tripPlanId") Long tripPlanId);
     
     List<Reserv> findByTripPlanIdAndMember(@Param("tripPlanId") Long tripPlanId, @Param("memberId") Long memberId);
-    
-//  List<ReservWithReviewDto> findAllWithReviewStatus(Long memberId);
 
     Reserv findReservByMemberAndTripPlan(@Param("tripPlanId") Long tripPlanId, @Param("memberId") Long memberId);
 
