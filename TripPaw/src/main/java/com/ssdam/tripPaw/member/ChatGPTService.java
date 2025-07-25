9ba94bbb8bcf23d63116a41780738d3747808247
sohyeon <syeonblue@gmail.com>
2025-07-25 16:03:12 +0900

    Update ChatGPTService.java

--- TripPaw/src/main/java/com/ssdam/tripPaw/member/ChatGPTService.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/member/ChatGPTService.java
@@ -21,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class ChatGPTService {
 
 	private final RestTemplate restTemplate;
+	@Value("${openai.api.key}")
 	private String apiKey;
 	
 	public ResponseEntity<String> chat(String message){
