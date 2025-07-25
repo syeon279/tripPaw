ec27c6ff4337dbb655aeac55a9b64912ff7f63aa
sohyeon <syeonblue@gmail.com>
2025-07-25 11:28:53 +0900

    Update UserInfoOauth2.java

--- TripPaw/src/main/java/com/ssdam/tripPaw/member/oauth/UserInfoOauth2.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/member/oauth/UserInfoOauth2.java
@@ -8,14 +8,3 @@ public interface UserInfoOauth2 {
 	String getImage();
 	
 }
-//com.company.project001.oauth
-//UserInfoOauth2
-//provider = "google", "kakao", "naver", "facebook"
-//providerId = google은 sub, kakao/facebook은 id, naver는 response 를 던져 줌
-//username = providerId설정
-//nickname = 각 사이트에서 등록한 이름으로 설정
-
-/*
- * 
- * 
- * */
