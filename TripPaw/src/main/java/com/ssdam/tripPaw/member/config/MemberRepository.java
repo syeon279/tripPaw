96387203150e9afb9c58a9dbad50a3dbf7d4de97
sohyeon <syeonblue@gmail.com>
2025-07-25 11:21:15 +0900

    Delete TripPaw/src/main/java/com/ssdam/tripPaw/member/config/MemberRepository.java

--- TripPaw/src/main/java/com/ssdam/tripPaw/member/config/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssdam.tripPaw.member.config;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssdam.tripPaw.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long>{
-
-}
