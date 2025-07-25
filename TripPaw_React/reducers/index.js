8474010a46dbe47eb0abaa329731834d58b7821a
sohyeon <syeonblue@gmail.com>
2025-07-25 15:38:05 +0900

    Update index.js

--- TripPaw_React/reducers/index.js
+++ TripPaw_React/reducers/index.js
@@ -1,13 +1,9 @@
 import { HYDRATE } from 'next-redux-wrapper';
 import { combineReducers } from 'redux';
 
-// 더미 reducer – 아무 동작 안 하지만 구조만 갖춤
-
 const dummy = (state = {}, action) => {
   return state;
 };
-
-// (이전상태, 액션) => 다음상태
 const rootReducer = (state, action) => {
   switch (action.type) {
     case HYDRATE:
