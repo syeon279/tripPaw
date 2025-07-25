ce5c92ebe0ad444a310614a7ddd78f68b83128b5
sohyeon <syeonblue@gmail.com>
2025-07-25 10:44:39 +0900

    Update ChatRoomMemberService.java

--- TripPaw/src/main/java/com/ssdam/tripPaw/chatting/chatRoomMember/ChatRoomMemberService.java
+++ TripPaw/src/main/java/com/ssdam/tripPaw/chatting/chatRoomMember/ChatRoomMemberService.java
@@ -17,7 +17,6 @@ public class ChatRoomMemberService {
 	public int insertChatRoomMember(Member member, ChatRoom chatRoom, ChatRole role, ChatRoomMemberStatus status) {
 		ChatRoomMember chatRoomMember;
 		if(role.name().equals("JOIN")) {
-			//chatRoomMemberMapper.findStatusByMemberIdAndChatRoomId(chatRoomMember);
 			chatRoomMember = ChatRoomMember.create(member, chatRoom, role, status);
 		}else {
 			chatRoomMember = ChatRoomMember.create(member, chatRoom, role, status);
